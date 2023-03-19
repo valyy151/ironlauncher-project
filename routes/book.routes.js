8524c0e94271015b3eb2d15a849e6cf5ad6378e0
valyy151 <marin.valenta1@gmail.com>
2023-03-19 14:22:04 +0100

    Added updating and deleting data from the database via DOM

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -4,7 +4,7 @@ const Book = require('../models/Book.model');
 router.get('/', (req, res) => {
 	Book.find()
 		.then((books) => {
-			res.render('books/books-list', { books });
+			res.render('books/list', { books });
 		})
 		.catch((err) => console.log('Error retrieving Books from the Database', err));
 });
@@ -12,13 +12,13 @@ router.get('/', (req, res) => {
 router
 	.route('/create')
 	.get((req, res) => {
-		res.render('books/book-create');
+		res.render('books/create');
 	})
 	.post((req, res) => {
 		const { title, author, rating, description } = req.body;
 		Book.create({ title, author, rating, description })
 			.then((data) => {
-				console.log('New Book Created: ', data);
+				console.log('New Book Created: ', data.title);
 				res.redirect('/books');
 			})
 			.catch((err) => console.log(err));
@@ -27,6 +27,26 @@ router
 router.get('/:bookId', (req, res) => {
 	Book.findById(req.params.bookId)
 		.then((book) => res.render('books/book', book))
+		.catch((err) => console.log(err));
+});
+
+router
+	.route('/:bookId/edit')
+	.get((req, res) => {
+		Book.findById(req.params.bookId)
+			.then((book) => res.render('books/edit', book))
+			.catch((err) => console.log(err));
+	})
+	.post((req, res) => {
+		const { title, author, rating, description } = req.body;
+		Book.findByIdAndUpdate(req.params.bookId, { title, author, rating, description }, { new: true })
+			.then((updatedBook) => res.redirect(`/books/${updatedBook.id}`))
+			.catch((err) => console.log(err));
+	});
+
+router.post('/:bookId/delete', (req, res) => {
+	Book.findByIdAndDelete(req.params.bookId)
+		.then((book) => res.redirect('/books'))
 		.catch((err) => console.log(err));
 });
 
